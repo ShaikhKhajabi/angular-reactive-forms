15eb10380fa0c83432abd08f3df65db41add4d40
Khajabi Shaikh <khajabi0410@gmail.com>
2022-05-09 14:58:35 +0530

    issue resolved after changing tsconfig

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+
+import { Customer } from './customer';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.scss']
+  styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm!: FormGroup ;
+  customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.customerForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      sendCatalog: true,
+    })
   }
 
+    populateTestData(): void{
+      this.customerForm.patchValue({
+        firstName: 'Bella',
+        lastName: 'Will',
+        sendCatalog: false,
+      });
+    }
+
+
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
 }
+
+
