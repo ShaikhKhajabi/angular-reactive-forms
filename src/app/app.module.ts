716160aabdb9dd9cdc866022ab7be90ebdc86f5d
Khajabi Shaikh <khajabi0410@gmail.com>
2022-05-09 09:03:33 +0530

    customer folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customers/customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule
