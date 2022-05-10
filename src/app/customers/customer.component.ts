34e02c9bb3d64729c31d52f0be46c20543d43490
Khajabi Shaikh <khajabi0410@gmail.com>
2022-05-10 10:45:26 +0530

    after many changes the project still not visible in the browser

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 
 import { Customer } from './customer';
@@ -17,7 +17,7 @@ export class CustomerComponent implements OnInit {
 
   ngOnInit() {
     this.customerForm = this.fb.group({
-      firstName: '',
+      firstName: ['',[Validators.required , Validators.minLength(3)]],
       lastName: '',
       email: '',
       sendCatalog: true,
