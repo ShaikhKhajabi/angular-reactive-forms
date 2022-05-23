5adb53f4c9ca131881edc989171e6be8658c607b
Khajabi Shaikh <khajabi0410@gmail.com>
2022-05-23 15:40:04 +0530

    custome validators and validators with parameter

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,8 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 
 import { Customer } from './customer';
+
+
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+   if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max )) {
+     return { 'range': true }
+   }
+   return null;
+  }
+}
 
 @Component({
   selector: 'app-customer',
@@ -20,6 +30,9 @@ export class CustomerComponent implements OnInit {
       firstName: ['',[Validators.required , Validators.minLength(3)]],
       lastName: ['', [Validators.required , Validators.maxLength(50)]],
       email: ['', [Validators.required , Validators.email]],
+      phone: '',
+      notification: 'email',
+      rating: [null, ratingRange(1,5)],
       sendCatalog: true,
     });
   }
@@ -37,6 +50,18 @@ export class CustomerComponent implements OnInit {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
+
+  setNotification(notifyVia: string): void{
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text'){
+      phoneControl.setValidators(Validators.required);
+    }
+    else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
+  }
+
 }
 
 
