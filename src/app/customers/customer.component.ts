978bc0d5333e640a68d816c01a4a384761be0cb5
Khajabi Shaikh <khajabi0410@gmail.com>
2022-06-10 11:40:14 +0530

    changed the email validation into reactive form

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,17 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  ValidatorFn,
+  FormArray
+} from '@angular/forms';
 
-
+import { debounceTime } from 'rxjs/operators';
 import { Customer } from './customer';
 
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
+  const emailControl = c.get('email');
+  const confrimEmail = c.get('confrimEmail');
+
+  if (emailControl.pristine || confrimEmail.pristine) {
+    return null;
+  }
+
+  if (emailControl.value === confrimEmail.value) {
+    return null;
+  }
+  return { match: true };
+}
 
 function ratingRange(min: number, max: number): ValidatorFn {
   return (c: AbstractControl): { [key: string]: boolean } | null => {
-   if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max )) {
-     return { 'range': true }
-   }
-   return null;
-  }
+    if (
+      c.value !== null &&
+      (isNaN(c.value) || c.value < min || c.value > max)
+    ) {
+      return { range: true };
+    }
+    return null;
+  };
+}
+
+class ValidationMessages {
+  [key: string]: string;
+  required: string;
+  email: string;
 }
 
 @Component({
@@ -22,19 +51,63 @@ function ratingRange(min: number, max: number): ValidatorFn {
 export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
   customer = new Customer();
+  emailMessage: string;
+
+  get addresses(): FormArray {
+    return <FormArray>this.customerForm.get('addresses') as FormArray;
+  }
+
+  private validationMessages: ValidationMessages = {
+    required: 'Please enter your email address.',
+    email: 'Please enter a valid email address',
+  };
+
+  // validationMessages.required
+  // validationMessages[0]
+  // validationMessages['required']
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.customerForm = this.fb.group({
-      firstName: ['',[Validators.required , Validators.minLength(3)]],
-      lastName: ['', [Validators.required , Validators.maxLength(50)]],
-      email: ['', [Validators.required , Validators.email]],
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        confirmEmail: ['', Validators.required],
+      }, { validator: emailMatcher }),
       phone: '',
       notification: 'email',
-      rating: [null, ratingRange(1,5)],
+      rating: [null, ratingRange(1, 5)],
       sendCatalog: true,
+      addresses: this.fb.array([this.buildAddress()])
     });
+
+    this.customerForm
+      .get('notification')
+      .valueChanges.subscribe((value) => this.setNotification(value));
+
+    const emailControl = this.customerForm.get('emailGroup.email');
+    emailControl.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(
+      value =>
+      this.setMessage(emailControl)
+    );
+  }
+
+  addAddress(): void{
+    this.addresses.push(this.buildAddress());
+  }
+
+  buildAddress(): FormGroup {
+    return this.fb.group({
+      addressType: 'home',
+    street1: '',
+    street2: '',
+    state: '',
+    zip: ''
+    })
   }
 
   populateTestData(): void {
@@ -45,23 +118,28 @@ export class CustomerComponent implements OnInit {
     });
   }
 
-
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
 
-  setNotification(notifyVia: string): void{
-    const phoneControl = this.customerForm.get('phone');
-    if (notifyVia === 'text'){
-      phoneControl.setValidators(Validators.required);
+  setMessage(c: AbstractControl): void {
+    this.emailMessage = '';
+    if ((c.touched || c.dirty) && c.errors) {
+      console.log(c);
+      this.emailMessage = Object.keys(c.errors)
+        .map((key: string) => this.validationMessages[key])
+        .join(' ');
     }
-    else {
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
       phoneControl.clearValidators();
     }
     phoneControl.updateValueAndValidity();
   }
-
 }
-
-
